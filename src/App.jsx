f3423b529ec9d60b63288cde459258aacd960ae0
Nelson Foltz <nelsfoltz@gmail.com>
2021-08-31 08:51:38 -0400

    add: useEffect with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+const firebase = process.env.REACT_APP_FIREBASE;
+
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch(`${firebase}/cart.json`, {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
